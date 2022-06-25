b0be0004bf88d6f8eb7f4ec5649fcb6a7df3d023
Harshit Shiroiya <43477266+harshitshiroiya@users.noreply.github.com>
2022-06-25 11:16:41 -0400

    Time: 480 ms (10.34%), Space: 17.8 MB (80.58%) - LeetHub

--- 109-convert-sorted-list-to-binary-search-tree/109-convert-sorted-list-to-binary-search-tree.py
+++ 109-convert-sorted-list-to-binary-search-tree/109-convert-sorted-list-to-binary-search-tree.py
@@ -11,20 +11,36 @@
 #         self.right = right
 class Solution:
     def sortedListToBST(self, head: Optional[ListNode]) -> Optional[TreeNode]:
-        def convertToArray(head):
-            arr = []
-            while head != None:
-                arr.append(head.val)
-                head = head.next
-            return arr
+#         def convertToArray(head):
+#             arr = []
+#             while head != None:
+#                 arr.append(head.val)
+#                 head = head.next
+#             return arr
         
-        def dfs(left, right):
-            if left > right: return None
-            mid = left + (right - left) // 2
-            root = TreeNode(arr[mid])
-            root.left = dfs(left, mid - 1)
-            root.right = dfs(mid + 1, right)
-            return root
+#         def dfs(left, right):
+#             if left > right: return None
+#             mid = left + (right - left) // 2
+#             root = TreeNode(arr[mid])
+#             root.left = dfs(left, mid - 1)
+#             root.right = dfs(mid + 1, right)
+#             return root
         
-        arr = convertToArray(head)
-        return dfs(0, len(arr)-1)
+#         arr = convertToArray(head)
+#         return dfs(0, len(arr)-1)
+        if not head:
+            return 
+        if not head.next:
+            return TreeNode(head.val)
+        slow, fast = head, head.next.next
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+        # tmp points to root
+        tmp = slow.next
+        # cut down the left child
+        slow.next = None
+        root = TreeNode(tmp.val)
+        root.left = self.sortedListToBST(head)
+        root.right = self.sortedListToBST(tmp.next)
+        return root
