b3630b09355a7e6a2a9d40e1e7bfbcfe4f859927
Harshit Shiroiya <43477266+harshitshiroiya@users.noreply.github.com>
2022-07-12 10:29:56 -0400

    Time: 1866 ms (37.68%), Space: 13.9 MB (60.42%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,20 +1,25 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        T = '#'.join('^{}$'.format(s))
-        n = len(T)
-        P = [0] * n
-        C = R = 0
-        for i in range (1, n-1):
-            P[i] = (R > i) and min(R - i, P[2*C - i]) # equals to i' = C - (i-C)
-            # Attempt to expand palindrome centered at i
-            while T[i + 1 + P[i]] == T[i - 1 - P[i]]:
-                P[i] += 1
-    
-            # If palindrome centered at i expand past R,
-            # adjust center based on expanded palindrome.
-            if i + P[i] > R:
-                C, R = i, i + P[i]
-    
-        # Find the maximum element in P.
-        maxLen, centerIndex = max((n, i) for i, n in enumerate(P))
-        return s[(centerIndex  - maxLen)//2: (centerIndex  + maxLen)//2]
+        res = ""
+        resLen = 0
+        
+        for i in range(len(s)):
+            # odd length
+            l, r = i, i
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > resLen:
+                    res = s[l:r+1]
+                    resLen = r - l + 1
+                l -= 1
+                r += 1
+            
+            # even length
+            l, r = i, i + 1
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > resLen:
+                    res = s[l:r+1]
+                    resLen = r - l + 1
+                l -= 1
+                r += 1
+                
+        return res
