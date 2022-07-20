587498f499156fcb519f572f915214420d04a1a9
Harshit Shiroiya <43477266+harshitshiroiya@users.noreply.github.com>
2022-07-19 23:41:11 -0400

    Time: 73 ms (88.60%), Space: 14 MB (43.79%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -5,12 +5,27 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        _ = l1.val + l2.val
-        digit, tenth = _ % 10, _ // 10
-        answer = ListNode(digit)
-        if any((l1.next, l2.next, tenth)):
-            l1 = l1.next if l1.next else ListNode(0)
-            l2 = l2.next if l2.next else ListNode(0)
-            l1.val += tenth
-            answer.next = self.addTwoNumbers(l1, l2)    
-        return answer
+        dummyHead = ListNode(0)
+        curr = dummyHead
+        carry = 0
+        while l1 != None or l2 != None or carry != 0:
+            l1Val = l1.val if l1 else 0
+            l2Val = l2.val if l2 else 0
+            columnSum = l1Val + l2Val + carry
+            carry = columnSum // 10
+            newNode = ListNode(columnSum % 10)
+            curr.next = newNode
+            curr = newNode
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+        return dummyHead.next
+        
+        # _ = l1.val + l2.val
+        # digit, tenth = _ % 10, _ // 10
+        # answer = ListNode(digit)
+        # if any((l1.next, l2.next, tenth)):
+        #     l1 = l1.next if l1.next else ListNode(0)
+        #     l2 = l2.next if l2.next else ListNode(0)
+        #     l1.val += tenth
+        #     answer.next = self.addTwoNumbers(l1, l2)    
+        # return answer
