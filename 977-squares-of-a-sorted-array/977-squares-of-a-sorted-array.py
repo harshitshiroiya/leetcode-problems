fdd9b304270d605c2963d5f7b74222b7113b3f25
Harshit Shiroiya <43477266+harshitshiroiya@users.noreply.github.com>
2022-07-26 19:29:09 -0400

    Time: 248 ms (85.01%), Space: 16.2 MB (49.26%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.py
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.py
@@ -1,12 +1,13 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        result = [None for _ in nums]
-        left, right = 0, len(nums) - 1
-        for index in range(len(nums)-1, -1, -1):
-            if abs(nums[left]) > abs(nums[right]):
-                result[index] = nums[left] ** 2
-                left += 1
-            else:
-                result[index] = nums[right] ** 2
+        left, right, new_arr = 0, len(nums)-1, []
+        
+        while left <= right:
+            if abs(nums[left]) <= abs(nums[right]):
+                new_arr.append(nums[right]**2)
                 right -= 1
-        return result
+            else:
+                new_arr.append(nums[left]**2)
+                left += 1
+            
+        return(new_arr[::-1])
