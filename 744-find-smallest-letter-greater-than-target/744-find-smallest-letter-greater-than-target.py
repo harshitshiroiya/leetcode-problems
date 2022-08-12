89660c3cb4994e927108529ab1ca462525e07219
Harshit Shiroiya <43477266+harshitshiroiya@users.noreply.github.com>
2022-08-12 11:14:34 +0400

    Time: 146 ms (69.04%), Space: 14.5 MB (20.86%) - LeetHub

--- 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.py
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.py
@@ -1,7 +1,6 @@
 class Solution:
     def nextGreatestLetter(self, letters: List[str], target: str) -> str:
-        seen = set(letters)
-        for i in range(1, 26):
-            cand = chr((ord(target) - ord('a') + i) % 26 + ord('a'))
-            if cand in seen:
-                return cand
+        for c in letters:
+            if c > target:
+                return c
+        return letters[0]
