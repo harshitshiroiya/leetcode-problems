e57ede3fffc72f5db1caa4e50a4ac54d3c693291
Harshit Shiroiya <43477266+harshitshiroiya@users.noreply.github.com>
2022-08-15 02:06:08 +0530

    Time: 55 ms (46.17%), Space: 14.2 MB (35.35%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,14 +1,20 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = []
-        subset = []
-        def DFS(i):
-            if i < 0:
-                res.append(subset.copy())
+        def explore(chosen, remaining, res):
+            if not remaining:
+                res.append(chosen[:])
                 return
-            subset.append(nums[i])
-            DFS(i - 1)
-            subset.pop()
-            DFS(i - 1)
-        DFS(len(nums) - 1)
+            d = remaining.pop(0)
+            #choose
+            chosen.append(d)
+            #explore
+            explore(chosen, remaining, res)
+            chosen.pop()
+            explore(chosen, remaining, res)
+            #unchoose
+            remaining.insert(0, d)
+        
+        res = []
+        chosen = []
+        explore(chosen, nums, res)
         return res
