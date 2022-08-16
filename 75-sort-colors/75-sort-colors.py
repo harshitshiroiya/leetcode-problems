f0cec61c9c054892e38e1175326ffb62aa98e39d
Harshit Shiroiya <43477266+harshitshiroiya@users.noreply.github.com>
2022-08-16 17:12:04 +0530

    Time: 67 ms (17.25%), Space: 13.8 MB (64.49%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -3,32 +3,7 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        RED, WHITE, BLUE = 0, 1, 2
-        
-        # two pointers for RED as well as BLUE
-        idx_red, idx_blue = 0, len(nums)-1
-        
-        i = 0
-        while i <= idx_blue :
-            
-            if nums[i] == RED:
-                
-                nums[idx_red], nums[i] = nums[i], nums[idx_red]
-                
-                # update idx for red
-                idx_red += 1
-            
-            
-            elif nums[i] == BLUE:
-            
-                nums[idx_blue], nums[i] = nums[i], nums[idx_blue]
-                
-                # update idx for blue
-                idx_blue -= 1
-                
-                # i-1 in order to stay and do one more color check on next iteration
-                i -= 1
-            
-            
-            # i move forward
-            i += 1                
+        for i in range(0, len(nums)):    
+            for j in range(i+1, len(nums)):    
+                if(nums[i] > nums[j]):    
+                    nums[j],nums[i] = nums[i],nums[j]
