4800595a3935354539b915cf1a814c8e065f13d2
Harshit Shiroiya <43477266+harshitshiroiya@users.noreply.github.com>
2022-08-24 18:08:00 +0530

    Time: 65 ms (51.28%), Space: 15.1 MB (98.72%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
@@ -6,20 +6,16 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        if root == None:
-            return int(0)
-        if root.left==None and root.right==None:
-            return int(1)
         depth = 0
-        arr = []
-        def find_depth(self,node :TreeNode,depth,arr):
-            if(node == None):
-                return
-            depth +=1
-            if node.left==None and node.right==None:
-                arr.append(depth)
-            find_depth(self,node.left,depth,arr)
-            find_depth(self,node.right,depth,arr)
-            return arr
-        find_depth(self,root,depth,arr)
-        return max(arr)
+        level = [root] if root else []
+        while level:
+            depth += 1
+            queue = []
+            for el in level:
+                if el.left:
+                    queue.append(el.left)
+                if el.right:
+                    queue.append(el.right)
+            level = queue
+            
+        return depth
